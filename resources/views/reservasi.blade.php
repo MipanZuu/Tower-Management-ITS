ed2a0665a3ea10bc3634bf967c496c0ffb33b0d6
Bagus Nugra <bagusnugrahaxd@gmail.com>
2023-01-24 20:53:21 +0700

    add stepper in reservasi

--- resources/views/reservasi.blade.php
+++ resources/views/reservasi.blade.php
@@ -15,6 +15,39 @@
 
  <!-- <h1>Reservasi Ruangan</h1> -->
 
+<div class="mt-6 ">
+<ol class="items-cente justify-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+    <li class="flex items-center text-blue-600 dark:text-blue-500 space-x-2.5">
+        <span class="flex items-center justify-center w-8 h-8 border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
+            1
+        </span>
+        <span>
+            <h3 class="font-medium leading-tight">Informasi Peminjaman</h3>
+            <p class="text-sm">Step details here<p>
+        </span>
+    </li>
+    <li class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
+        <span class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+            2
+        </span>
+        <span>
+            <h3 class="font-medium leading-tight">Company info</h3>
+            <p class="text-sm">Step details here<p>
+        </span>
+    </li>
+    <li class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
+        <span class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+            3
+        </span>
+        <span>
+            <h3 class="font-medium leading-tight">Payment info</h3>
+            <p class="text-sm">Step details here<p>
+        </span>
+    </li>
+</ol>
+</div>
+
+
   <div class="container w-full flex flex-wrap mx-auto px-2 pt-8 lg:pt-4 mt-8">
         <!--Section container-->
         <section class="w-full">
