45f41a1be201c30a68bd58f8e41b2f860f02409c
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2023-02-13 17:16:12 +0700

    fixing relative view

--- resources/views/staffdisplay.blade.php
+++ resources/views/staffdisplay.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.header')
 @section('content')
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+<div class="overflow-x-auto shadow-md sm:rounded-lg h-screen">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-            Our products
-            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
+            Staff 
+            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus ex repellat ipsum, voluptatibus voluptate vel ab natus quis fugiat consequatur, enim assumenda eveniet sunt reprehenderit odit non dolore eum est.</p>
         </caption>
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
