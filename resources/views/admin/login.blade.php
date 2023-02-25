02067235326981760174f3605a1e1a32f1177d3d
Denta Bramasta Hidayat <denta.dent53@gmail.com>
2023-02-25 21:38:21 +0700

    deleted sign up

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -53,9 +53,6 @@
                       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
                   <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <!-- <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don't have an account yet? <a href="{{route('viewSignUp')}}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                  </p> -->
               </form>
               @foreach($errors->all() as $error)
                 <div class="error py-2 text-sm text-red-600">{{ $errors }}</div>
